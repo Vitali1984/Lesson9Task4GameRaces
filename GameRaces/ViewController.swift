af53897d9fcadc132ee7d6eb8795c56e280f4b5a
Vitali1984 <vitali.kuchinski1984@gmail.com>
2022-02-01 16:52:09 +0300

    add project and start screen

---
+++ GameRaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameRaces
+//
+//  Created by Виталий on 1.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
